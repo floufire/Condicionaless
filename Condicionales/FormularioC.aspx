672a66299a2310661c167afb1b7003bd805da8a1
Santiago Zapata Pineda <santizapata2019@gmail.com>
2020-08-04 23:33:15 -0500

    iPrimerCommit

---
+++ Condicionales/FormularioC.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="FormularioC.aspx.vb" Inherits="Condicionales.FormularioC" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td>
+                        <asp:Label ID="Etiqueta1" runat="server" Text="Cantidad de Llantas:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tllantas" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="BCalcular" runat="server" Text="Calcular" />
+                    </td>
+                    <td>
+                        <asp:Label ID="Respuesta" runat="server"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Condicionales/FormularioC.aspx.designer.vb
@@ -0,0 +1,60 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class FormularioC
+
+    '''<summary>
+    '''Control form1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''Control Etiqueta1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Etiqueta1 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control tllantas.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents tllantas As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control BCalcular.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents BCalcular As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control Respuesta.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Respuesta As Global.System.Web.UI.WebControls.Label
+End Class
