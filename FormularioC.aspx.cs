c10c0da12b020387d6c40a07f2c86dc0a5080d2e
Santiago Zapata Pineda <santizapata2019@gmail.com>
2020-08-05 13:00:11 -0500

    Tercer Commit

---
+++ FormularioC.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CondicionalesS
+{
+    public partial class FormularioC : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BCalcular_Click(object sender, EventArgs e)
+        {
+            int qllantas = System.Convert.ToInt32(tllantas.Text);
+            int resultado;
+
+            if (qllantas>=0 && qllantas < 5)
+            {
+                 resultado = qllantas * 800;
+                
+            }
+            else
+            {
+                 resultado = qllantas * 700;
+            }
+
+            Respuesta.Text = System.Convert.ToString("El precio es de $" + resultado);
+        }
+    }
+}
---
+++ FormularioC.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace CondicionalesS
+{
+
+
+    public partial class FormularioC
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control Etiqueta1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Etiqueta1;
+
+        /// <summary>
+        /// Control tllantas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tllantas;
+
+        /// <summary>
+        /// Control BCalcular.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BCalcular;
+
+        /// <summary>
+        /// Control Respuesta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Respuesta;
+    }
+}
